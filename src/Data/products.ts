c66400f4302f04fcd3e620bb7287a5069648dc7b
abay777 <abaysankar777@gmailcom>
2024-03-25 16:23:52 +0530

    added global state

--- src/Data/products.ts
+++ src/Data/products.ts
@@ -5,13 +5,15 @@ import protienpowder from '../assets/product Protein-Powder.png'
 import giftCard from '../assets/product gift-card.png'
 import whitetsirt from '../assets/product white tshirt.png'
 
-interface product {
-    productName :string;
+export interface  product {
+        productName :string;
         price:string;
         cost:number;
-        pack:string[],
+        pack:string[]| number[],
         features:string[],
-        productImg:string
+        productImg:string,
+        id:number
+        calc:boolean
 }
 
 
@@ -23,58 +25,70 @@ export const products:product[] = [
         cost:25,
         pack:['Celery, Spinach, Ginger','Carrot, Apple, Mango','Watermelon, Coconut'],
         features:['• Vegan','• Gluten-free','• No GMO’s','• No sugar added'],
-        productImg:pressedJuice
+        productImg:pressedJuice,
+        id:Math.ceil(Math.random()*1020),
+        calc:false,
     },
     {
         productName :'Protein Bars',
         price:'$36.00 every month',
         cost:36,
-        pack:['6' , '12' , '24'],
+        pack:[6 , 12 , 24],
         features:[
             '• 25g of protein per serving' ,'• Grass fed, non-GMO' ,'• No GMO’s' ,
             '• Soy Free and Gluten Free' ,'• No rBGH or rBST' ,'• No artificial colors, sweeteners or flavors'
             ,'• Zero added sugar'
         ],
-        productImg:proteinBar
+        productImg:proteinBar,
+        id:Math.ceil(Math.random()*1022),
+        calc:true,
     },
     {
         productName :'Endurance T-Shirt',
         price:'$30.00',
         cost:30,
-        pack:['0'],
+        pack:[2,4,6,10],
         features:[
             '• 100% organic cotton',
             '• Pre-washed'
         ],
-        productImg:Enduranceshirt
+        productImg:Enduranceshirt,
+        id:Math.ceil(Math.random()*10245),
+        calc:true,
     },
     {
         productName :'Protein Powder',
         price:'$30.00 every month',
         cost:30,
-        pack:['0'],
+        pack:[],
         features:[
             '• 25g of protein per serving','• Grass fed, non-GMO','• Soy Free and Gluten Free',
             '• No rBGH or rBST','• Cold processed','• No artificial colors, sweeteners or flavors',
             '• Zero added sugar'
         ],
-        productImg:protienpowder
+        productImg:protienpowder,
+        id:Math.ceil(Math.random()*1021),
+        calc:false,
     },
     {
         productName :'Klipsan T-Shirt',
         price:'$25.00',
         cost:30,
-        pack:['$25.00','$50.00','$75.00','$100.00'],
+        pack:[1,3,6,10],
         features:['• 100% organic cotton' , '• Pre-washed'],
-        productImg:whitetsirt
+        productImg:whitetsirt,
+        id:Math.ceil(Math.random()*10220),
+        calc:true,
     },
     {
         productName :'Gift Card',
         price:'from $25.00',
         cost:25,
-        pack:['$25.00','$50.00','$75.00','$100.00'],
+        pack:[25 , 50, 75 , 100],
         features:['This gift card never expires'],
-        productImg:giftCard
+        productImg:giftCard,
+        id:Math.ceil(Math.random()*10202),
+        calc:true,
     }
     
     
--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Contact, Schedules, Store, Instructors, Classes, Prices } from './components'
+import { Contact, Schedules, Store, Instructors, Classes, Prices, Product } from './components'
+import { Provider } from 'react-redux'
+import { store } from './state/store.ts'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <Routes>
-        <Route path='/' element={<App/>}/>
-        <Route path='/Schedule' element={<Schedules/>}/>
-        <Route path='/Contact' element={<Contact/>} />
-        <Route path='/Store' element={<Store/>}/>
-        <Route path='/Instructors' element={<Instructors/>}/>
-        <Route path='/Classes' element={<Classes/>}/>
-        <Route path='/Prices' element={<Prices/>}/>
+       
+          <Route path='/' element={<App/>}/>
+          <Route path='/Schedule' element={<Schedules/>}/>
+          <Route path='/Contact' element={<Contact/>} />
+          <Route path='/Store' element={<Store/>}/>
+          <Route path='/Instructors' element={<Instructors/>}/>
+          <Route path='/Classes' element={<Classes/>}/>
+          <Route path='/Prices' element={<Prices/>}/>
+          <Route path={`/Store/:id`} element={<Product/>}/>
+        
+        
       </Routes>
     </Router>
+  </Provider>
   </React.StrictMode>
 )
